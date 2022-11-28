34d57ffbc83cdeede9124074ace46d2527d8129c
Alca <karenrivasg17@gmail.com>
2022-11-27 23:24:44 -0600

    php implementation

---
+++ index.php
@@ -0,0 +1,93 @@
+<?php include("layout/header.php"); ?>
+
+  <section class="container mt-5">
+    <div class="row align-items-center">
+      <div class="col-lg-5">
+        <h1 class="title">Ready to try the best chinese flavors?</h1>
+        <p class="mt-5 mb-5 simpleText">Jump right in and try the best noodles and flavors combinations on our chinese
+          menu!</p>
+        <a class="buttonStyle" href="#">Our menu
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right"
+            viewBox="0 0 16 16">
+            <path fill-rule="evenodd"
+              d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+          </svg>
+        </a>
+      </div>
+
+      <div class="col-lg-7">
+        <img src="resources/bowl.png" width="100%" alt="">
+      </div>
+    </div>
+  </section>
+
+  <section class="container mt-5">
+    <div class="row align-items-center text-center justify-content-center">
+      <h1 class="title mt-4">About us</h1>
+      <div class="col-md-2">
+        <img src="resources/logo-text.png" width="100px" alt="">
+      </div>
+      <div class="col-md-9 ">
+
+        <p class="simpleText mt-5">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ultrices lorem turpis, vitae pharetra sem
+          ultricies at. Nullam consequat erat in mauris iaculis, eu convallis mauris dapibus. Sed sed viverra dolor.
+          Duis sit amet ex et justo tempor interdum. Mauris sodales enim non sem sagittis scelerisque. Fusce id tempus
+          nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin vitae est
+          id risus mollis fringilla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+          himenaeos. Duis ac lacinia felis. Vestibulum faucibus tellus nec mi feugiat suscipit. Cras non erat dictum,
+          scelerisque sapien sed, facilisis risus. Donec venenatis finibus sapien eu ornare. Maecenas et nisl id lacus
+          semper ultrices. Nunc aliquam eros nec dignissim rhoncus.
+        </p>
+      </div>
+      <div class="col-md-11">
+        <p class="simpleText mt-5">
+          Interdum et malesuada fames ac ante ipsum primis in
+          faucibus.
+          Aliquam fringilla eleifend lectus, non rhoncus mauris ornare sit amet. Phasellus efficitur elementum lacus
+          dapibus hendrerit. Ut ac gravida lacus. Maecenas eu augue quis velit laoreet sodales. Phasellus hendrerit
+          mauris ligula, eget efficitur tellus mattis ac. Nunc in nisl vitae nunc elementum elementum vel eget dolor.
+          Nulla ut elementum purus. Nunc tincidunt, urna at pulvinar ullamcorper, justo turpis elementum metus, non
+          luctus diam nisi eu ex. Cras vitae dui vitae risus tincidunt scelerisque id sed mi. In nec quam est. Integer
+          porta posuere quam et rhoncus. Ut luctus tellus vitae rhoncus convallis. Nullam rhoncus facilisis iaculis. Nam
+          sed dignissim justo, non tincidunt mi.
+        </p>
+      </div>
+    </div>
+  </section>
+
+  <section class="mt-5">
+    <div class="container">
+      <h1 class="title mt-4 text-center">Our menu</h1>
+      <div class="row justify-content-center mt-5">
+        
+        <div class="card col-md-3 mx-4 my-3">
+          <img src="resources/israel-albornoz-A7xuEYKN7a4-unsplash.jpg" class="card-img-top" alt="food">
+          <div class="card-body">
+            <div class="row">
+              <h4 class="card-title">Red sushi</h4>
+              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            </div>
+            <p class="card-text simpleText hidden">Praesent vitae eleifend orci. Cras laoreet est aliquam elit lobortis, ut
+              molestie massa aliquam. Vivamus non
+              pretium urna, ut venenatis metus. Proin posuere ultrices ante.</p>
+          </div>
+        </div>
+
+        <div class="cardStyle col-md-3 mx-4 my-3">
+          <img src="resources/israel-albornoz-A7xuEYKN7a4-unsplash.jpg" class="card-img-top" alt="food">
+          <div class="card-body">
+            <div class="row">
+              <h4 class="card-title">Red sushi</h4>
+              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            </div>
+            <p class="card-text simpleText hidden">Praesent vitae eleifend orci. Cras laoreet est aliquam elit lobortis, ut
+              molestie massa aliquam. Vivamus non
+              pretium urna, ut venenatis metus. Proin posuere ultrices ante.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <?php include("template/footer.php"); ?>
--- layout/header.php
+++ layout/header.php
@@ -43,21 +43,3 @@
       </div>
     </nav>
   </header>
-</body>
-
-  
-
-  <footer>
-    <p class="card-text simpleText mt-5">Some quick example text to build on the card title and make up the bulk of the
-      card's content.</p>
-  </footer>
-<frameset cols="100%, *">
-  <frame src="userContent.html"/>
-</frameset>
-<noframes>
-
-</noframes>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-  integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-
-</html>
